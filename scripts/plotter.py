373d872a5ece0ffe1c577453dd33ca9e07fdc918
chris-revell <cr395@cam.ac.uk>
2018-11-21 13:23:41 -0500

    Basic functioning model

---
+++ scripts/plotter.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+data = np.genfromtxt("output/cellpositions.txt")
+ncells = np.genfromtxt("output/cellcount.txt",dtype=int)
+xmax=100
+drawn = 0
+for step in range(ncells.shape[0]):
+    print("{:02d}/{:02d}".format((step+1),ncells.shape[0]))
+    fig, ax = plt.subplots(figsize=(6,6))
+    print("{}:{}".format(drawn,drawn+ncells[step]))
+
+    ax.scatter(data[drawn:drawn+ncells[step],0],data[drawn:drawn+ncells[step],1])
+    drawn = drawn+ncells[step]
+    ax.set_xlim([-xmax,xmax])
+    ax.set_ylim([-xmax,xmax])
+    ax.tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False)
+    ax.tick_params(axis='y',which='both',left=False,right=False,labelleft=False)
+    fig.savefig("output/test{:05d}".format(step))
+    plt.close()
+os.system("convert -delay 10 -loop 0 output/*.png output/animated.gif;")
+# rm output/*.png")
--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,10 @@ void MorseForce(cell& Cell1,cell& Cell2){
   F = 2*a*De*(expfactor-pow(expfactor,2));
   vx = F*dx/r;
   vy = F*dy/r;
-  Cell1.v(0) = vx;
-  Cell1.v(1) = vy;
-  Cell2.v(0) = -vx;
-  Cell2.v(1) = -vy;
+  Cell1.v(0) = Cell1.v(0)+vx;
+  Cell1.v(1) = Cell1.v(1)+vy;
+  Cell2.v(0) = Cell2.v(0)-vx;
+  Cell2.v(1) = Cell2.v(1)-vy;
 }
 
 int PositionToIndex(const cell& Cell,const float& griddim,const int& Ng,const int& dim){
@@ -36,10 +36,13 @@ int PositionToIndex(const cell& Cell,const float& griddim,const int& Ng,const in
 }
 
 void CellDivision(vector<cell>& Cells,int& Nc,const float& cellradius,const float& cellcycletime){
+  float theta;
   for (int ii=0;ii<Nc;ii++){
     if (Cells[ii].age>cellcycletime){
-      Cells.push_back(cell(Cells[ii].pos(0)+cellradius,Cells[ii].pos(1)));
-      Cells[ii].pos(0) = Cells[ii].pos(0)-cellradius;
+      theta = 2*M_PI*rand()/RAND_MAX;
+      Cells.push_back(cell(Cells[ii].pos(0)+cellradius*cos(theta),Cells[ii].pos(1)+cellradius*sin(theta)));
+      Cells[ii].pos(0) = Cells[ii].pos(0)-cellradius*cos(theta);
+      Cells[ii].pos(1) = Cells[ii].pos(1)-cellradius*sin(theta);
       Cells[ii].age = 0;
     }else{}
   }
@@ -70,43 +73,25 @@ int main(){
   Cells.push_back(cell(0,0));
 
   while (t<t_max){
-    //morseforce(Cells[0],Cells[1]);
-    //Cells[0].pos = Cells[0].pos+Cells[0].v;
-    //Cells[1].pos = Cells[1].pos+Cells[1].v;
-    cout << "test a" << endl;
     CellDivision(Cells,Nc,cellradius,cellcycletime);
-    cout << "test b" << endl;
     gridcount.zeros();
-    cout << "test c" << endl;
-    cout << Nc << endl;
     for (int ii=0;ii<Nc;ii++){
-      cout << "test 1aa" << endl;
       ix = PositionToIndex(Cells[ii],griddim,Ng,0);
-      cout << "test 1a "<< endl;
       iy = PositionToIndex(Cells[ii],griddim,Ng,1);
-      cout << "test 1b " << ix << " " << iy << endl;
-      cout << "gc " << gridcount(ix,iy) << endl;
       gridcells(ix,iy,gridcount(ix,iy))=ii;
-      cout << "test 1c" << endl;
       gridcount(ix,iy)++;
+      Cells[ii].v.zeros();
     }
-    cout << "test d" << endl;
     for (int ii=0;ii<Nc;ii++){
-      cout << "test 1" << endl;
       ix = PositionToIndex(Cells[ii],griddim,Ng,0);
       iy = PositionToIndex(Cells[ii],griddim,Ng,1);
-      cout << "test e" << endl;
       for (int jj=-1;jj<2;jj++){
-        cout << "test 1" << endl;
         for (int kk=-1;kk<2;kk++){
-          cout << "test 2" << endl;
           if (((ix+jj)>=Ng)||((ix+jj)<0)||((iy+kk)>=Ng)||((iy+kk)<0)){
           }else{
-            cout << "test 3" << endl;
             for (int ll=0;ll<gridcount(ix+jj,iy+kk);ll++){
               if (gridcells(ix+jj,iy+kk,ll)!=ii){
                 MorseForce(Cells[ii],Cells[gridcells(ix+jj,iy+kk,ll)]);
-                cout << "test 4" << endl;
               }else{}
             }
           }
@@ -114,17 +99,14 @@ int main(){
       }
       Cells[ii].age = Cells[ii].age+dt;
     }
-    cout << "test 5" << endl;
     for (int ii=0;ii<Nc;ii++){
-      Cells[ii].pos = Cells[0].pos+dt*Cells[0].v;
-      cout << "test 6" << endl;
+      Cells[ii].pos = Cells[ii].pos+dt*Cells[ii].v;
       outfile1 << Cells[ii].pos(0) << " " << Cells[ii].pos(1) << endl;
     }
-    cout << "test 7" << endl;
     outfile2 << Nc << endl;
 
     t=t+dt;
-
+    cout << t << endl;
   }
 
   return 0;

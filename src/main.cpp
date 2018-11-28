744ad8b482065749fb3148884fc06210cd8dff17
chris-revell <cr395@cam.ac.uk>
2018-11-28 08:19:54 +0000

    CellDivision module

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,10 @@
 #include <cell.hpp>
 #include <MorseForce.hpp>
 #include <PositionToIndex.hpp>
+#include <CellDivision.hpp>
 
 using namespace std;
 using namespace arma;
-
-// Subroutine to test all cells in vector Cells and determine whether any are older than the cell cycle time.
-// Any cells older than the cell cycle time undergo division. The two daughter cells have age=0
-// and are always separated by 2* typical cell radius.
-void CellDivision(vector<cell>& Cells,int& Nc,const float& cellradius,const float& cellcycletime){
-  float theta;
-  default_random_engine generator;
-  normal_distribution<double> distribution(0.0,0.01);
-  for (int ii=0;ii<Nc;ii++){
-    if (Cells[ii].age>cellcycletime){
-      //theta = 2*M_PI*rand()/RAND_MAX;
-      theta = 2*M_PI*distribution(generator);
-      Cells.push_back(cell(Cells[ii].pos(0)+cellradius*cos(theta),Cells[ii].pos(1)+cellradius*sin(theta)));
-      Cells[ii].pos(0) = Cells[ii].pos(0)-cellradius*cos(theta);
-      Cells[ii].pos(1) = Cells[ii].pos(1)-cellradius*sin(theta);
-      Cells[ii].age = 0;
-    }else{}
-  }
-  Nc=Cells.size();
-}
 
 int main(){
   vector<cell> Cells;                   // Vector containing cell objects corresponding to all cells in the system
@@ -94,7 +75,7 @@ int main(){
       Cells[ii].age = Cells[ii].age+dt;
     }
 
-    if (fmod(t,10.0)<dt){
+    if (fmod(t,5)<dt){
       cout << t << " " << fmod(t,10.0) << endl;
       // Update all cell positions according to cell velocities and write positions to file
       for (int ii=0;ii<Nc;ii++){

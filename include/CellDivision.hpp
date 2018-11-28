3b1e367ebc625b137cd0e28c05528774632e447b
chris-revell <cr395@cam.ac.uk>
2018-11-28 08:19:54 +0000

    CellDivision module

---
+++ include/CellDivision.hpp
@@ -0,0 +1,17 @@
+//
+//  CellDivision.hpp
+//  anisotropic-division
+//
+//  Created by Christopher Revell on 27/11/2018.
+//
+//
+
+#ifndef CellDivision_hpp
+#define CellDivision_hpp
+
+#include <vector>
+#include <cell.hpp>
+
+void CellDivision(std::vector<cell>& Cells,int& Nc,const float& cellradius,const float& cellcycletime);
+
+#endif /* CellDivision_hpp */
